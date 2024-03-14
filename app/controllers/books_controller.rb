966674ce112eb222241962eeb0a2d9477422d656
MoeMaeda <160582011+moe-maeda@users.noreply.github.com>
2024-03-14 15:07:25 +0000

    [Add] 4-27 createアクション設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new
-    @book.save
-    redirect_to about_path
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to books_path
+    else
+      render:new
+    end
   end
 
   def index
@@ -24,4 +28,12 @@ class BooksController < ApplicationController
 
   def destroy
   end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
