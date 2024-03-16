53d244b305b01f4b461d7765748f8fdacd93b57c
MoeMaeda <160582011+moe-maeda@users.noreply.github.com>
2024-03-16 07:48:02 +0000

    [Remove] 4-27 book.indexページの不具合修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,24 +1,24 @@
 class BooksController < ApplicationController
 
-  def new
-    @book = Book.new
-  end
-
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to books_path
+      redirect_to book_path(@book.id)
     else
-      render:index
+      render:show
     end
   end
 
   def index
     @books = Book.all
+    @user = current_user
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
+    @user = current_user
   end
 
   def edit
@@ -28,6 +28,9 @@ class BooksController < ApplicationController
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
 
