300163d97f6f1b0bcbe250313a6ea7d67abb36d4
MoeMaeda <160582011+moe-maeda@users.noreply.github.com>
2024-03-18 05:03:45 +0000

    [Add] 4-27 フラッシュメッセージ設定完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
     @book_new = Book.new(book_params)
     @book_new.user_id = current_user.id
     if @book_new.save
+      flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book_new.id)
     else
       render :show
@@ -12,13 +13,13 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.includes(:user).all
-    @user_in = current_user
+    @user_info = current_user
     @book_new = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
-    @user_in = current_user
+    @user_info = @book.user
     @book_new = Book.new
   end
 
@@ -29,6 +30,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
       redirect_to book_path(@book.id)
     else
       render :edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,51 @@
 class UsersController < ApplicationController
 
+  def create
+    @user_sign_up = User.new(user_params)
+    if @user_sign_up.save
+      flash[:notice] = "Welcome! You have signed up successfully."
+    end
+    @user = login(params[:name], params[:password])
+    if @user
+      flash[:notice] = "Signed in successfully."
+    end
+  end
+
   def index
     @users = User.all
-    @user_in = current_user
+    @user_info = current_user
+    @book_new = Book.new
   end
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
-    @user_in = current_user
+    @user_info = @user
+    @book_new= Book.new
   end
 
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
-  
+
+  def destroy
+    logout
+    flash[:notice] = "Signed out successfully."
+  end
+
   private
-  
+
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
